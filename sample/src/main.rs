ef4e866a97301b11957d6ebd4893cd91350e0efa
Kouhei Aoyagi <aoyagi_kouhei@uniquevision.co.jp>
2025-03-12 08:35:49 +0900

    modify command

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -9,13 +9,22 @@ pub mod schema;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    move_csv::execute("../input.csv", "output.parquet")?;
-    read_all::execute("output.parquet")?;
-    filter::execute("output.parquet").await?;
-    group::execute("output.parquet").await?;
-    maker::execute("result.parquet").await?;
-    read_all::execute("result.parquet")?;
-    group::execute("result.parquet").await?;
+    let command = std::env::var("COMMAND")?;
+    match command.as_str() {
+        // COMMAND=move_csv cargo run
+        // duckdb
+        // select * from 'output.parquet';
+        "move_csv" => move_csv::execute("../input.csv", "output.parquet")?,
+        // COMMAND=read_all cargo run
+        "read_all" => read_all::execute("output.parquet")?,
+        // COMMAND=filter cargo run
+        "filter" => filter::execute("output.parquet").await?,
+        // COMMAND=group cargo run
+        "group" => group::execute("output.parquet").await?,
+        // COMMAND=maker cargo run
+        "maker" => maker::execute("result.parquet").await?,
+        _ => panic!("Unknown command: {}", command),
+    }
 
     Ok(())
 }
